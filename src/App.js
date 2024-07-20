d9bf41f945a0a638c5f1bef87f7c6a0b5babf3c7
Sal-Mit <salonimittal2200@gmail.com>
2024-07-20 11:17:28 -0400

    updated navbar, footer, added .gitignore file

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import NavBar from './customer/component/navbar/NavigationBar';
 import HomePage from './customer/Pages/HomePage';
 import Footer from './customer/component/Footer/footer';
 import Product from './customer/component/Product/Product';
+
+
 function App() {
   return (
   <div>
     <NavBar ></NavBar>
   <div>
-  { /* <HomePage/>  */ }  
-    <Product></Product>
+  {  <HomePage/>   }  
+    {/* <Product></Product> */}
   </div>
       <Footer />
   </div> 
--- src/customer/component/navbar/NavigationBar.jsx
+++ src/customer/component/navbar/NavigationBar.jsx
@@ -13,7 +13,7 @@ import {
   TabPanel,
   TabPanels,
 } from '@headlessui/react'
-import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon, UserIcon, HeartIcon } from '@heroicons/react/24/outline'
 
 const navigation = {
   categories: [
@@ -41,8 +41,38 @@ const navigation = {
           items: [
             { name: 'Tops', href: '#' },
             { name: 'Dresses', href: '#' },
+            { name: 'T-Shirts', href: '#' },
+            { name: 'Browse All', href: '#' },
+          ],
+        },
+
+      ],
+    },
+    {
+      id: 'men',
+      name: 'Men',
+      featured: [
+        {
+          name: 'New Arrivals',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg',
+          imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
+        },
+        {
+          name: 'Artwork Tees',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg',
+          imageAlt:
+            'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
+        },
+      ],
+      sections: [
+        {
+          id: 'clothing',
+          name: 'Clothing',
+          items: [
+            { name: 'Tops', href: '#' },
             { name: 'Pants', href: '#' },
-            { name: 'Denim', href: '#' },
             { name: 'Sweaters', href: '#' },
             { name: 'T-Shirts', href: '#' },
             { name: 'Jackets', href: '#' },
@@ -50,23 +80,33 @@ const navigation = {
             { name: 'Browse All', href: '#' },
           ],
         },
-      
+        {
+          id: 'accessories',
+          name: 'Accessories',
+          items: [
+            { name: 'Watches', href: '#' },
+            { name: 'Wallets', href: '#' },
+            { name: 'Bags', href: '#' },
+            { name: 'Sunglasses', href: '#' },
+            { name: 'Hats', href: '#' },
+            { name: 'Belts', href: '#' },
+          ],
+        },
         {
           id: 'brands',
           name: 'Brands',
           items: [
-            { name: 'Full Nelson', href: '#' },
-            { name: 'My Way', href: '#' },
             { name: 'Re-Arranged', href: '#' },
             { name: 'Counterfeit', href: '#' },
-            { name: 'Significant Other', href: '#' },
+            { name: 'Full Nelson', href: '#' },
+            { name: 'My Way', href: '#' },
           ],
         },
       ],
     },
     {
-      id: 'men',
-      name: 'Men',
+      id: 'kids',
+      name: 'Kids',
       featured: [
         {
           name: 'New Arrivals',
@@ -262,10 +302,6 @@ export default function NavBar() {
       </Dialog>
 
       <header className="relative bg-yellow-300">
-      <p className="flex h-10 items-center justify-center bg-black px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
-          Get free delivery on orders over $100
-      </p>
-
         <nav aria-label="Top" className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="border-b border-gray-200">
             <div className="flex h-16 items-center">
@@ -279,28 +315,17 @@ export default function NavBar() {
                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
               </button>
 
-              {/* Logo */}
-              {/*}
+              {/* Logo */}              
               <div className="ml-4 flex lg:ml-0">
                 <a href="#">
-                  <span className="sr-only">Saloni Styles</span>
-    
                   <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt=""
-                  />
-                 
+                    className="h-10 w-auto"
+                    src="https://www.vitecinc.com/wp-content/uploads/2016/04/not-giant-enough-letter-s.jpg"
+                    alt="Loading"
+                  />            
                 </a>
               </div>
-              */}
-               <div className="ml-4 flex lg:ml-0">
-                <a href="#" className="text-2xl font-bold text-gray-900 uppercase tracking-widest">
-                  Saloni Styles
-                </a>
-              </div>
-
-
+              
 
               {/* Flyout menus */}
               <PopoverGroup className="hidden lg:ml-8 lg:block lg:self-stretch">
@@ -382,20 +407,25 @@ export default function NavBar() {
                       )}
                     </Popover>
                   ))}
-
-                  {navigation.pages.map((page) => (
-                    <a
-                      key={page.name}
-                      href={page.href}
-                      className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
-                    >
-                      {page.name}
-                    </a>
-                  ))}
                 </div>
               </PopoverGroup>
+              
+              {/* Search */}
+              <form className="flex items-center max-w-lg mx-auto"> 
+                <label htmlFor="search" className="sr-only">Search</label>
+                <div className="relative w-full">
+                  <button type="submit" className="absolute inset-y-0 left-0 flex items-center pl-3 cursor-pointer pointer-events-auto">
+                    <MagnifyingGlassIcon className="h-5 w-5 text-gray-400 hover:text-gray-500" aria-hidden="true" />
+                  </button>
+                  <input type="search" id="default-search" placeholder="Search" className="p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 " required />
+                </div>
+              </form>
+
+                
 
               <div className="ml-auto flex items-center">
+
+                {/* Create Account/ Log In */}
                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                   <a href="#" className="text-sm font-medium text-gray-700 hover:text-gray-800">
                     Sign in
@@ -406,23 +436,23 @@ export default function NavBar() {
                   </a>
                 </div>
 
-                <div className="hidden lg:ml-8 lg:flex">
-                  <a href="#" className="flex items-center text-gray-700 hover:text-gray-800">
-                    <img
-                      src="https://tailwindui.com/img/flags/flag-canada.svg"
-                      alt=""
-                      className="block h-auto w-5 flex-shrink-0"
+                {/* Profile */}
+                <div className="ml-4 flow-root lg:ml-6">
+                  <a href="#" className="group -m-2 flex items-center p-2">
+                    <UserIcon
+                      className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:fill-gray-500"
+                      aria-hidden="true"
                     />
-                    <span className="ml-3 block text-sm font-medium">CAD</span>
-                    <span className="sr-only">, change currency</span>
                   </a>
                 </div>
 
-                {/* Search */}
-                <div className="flex lg:ml-6">
-                  <a href="#" className="p-2 text-gray-400 hover:text-gray-500">
-                    <span className="sr-only">Search</span>
-                    <MagnifyingGlassIcon className="h-6 w-6" aria-hidden="true" />
+                {/* Wishlist */}
+                <div className="ml-4 flow-root lg:ml-6">
+                  <a href="#" className="group -m-2 flex items-center p-2">
+                    <HeartIcon
+                      className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:fill-gray-500"
+                      aria-hidden="true"
+                    />
                   </a>
                 </div>
 
@@ -430,13 +460,14 @@ export default function NavBar() {
                 <div className="ml-4 flow-root lg:ml-6">
                   <a href="#" className="group -m-2 flex items-center p-2">
                     <ShoppingBagIcon
-                      className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                      className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:fill-gray-500"
                       aria-hidden="true"
                     />
                     <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
                     <span className="sr-only">items in cart, view bag</span>
                   </a>
                 </div>
+
               </div>
             </div>
           </div>
