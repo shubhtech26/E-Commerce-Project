07f7926dce35a7cb786ce84772bea3dc2b6138aa
Sal-Mit <salonimittal2200@gmail.com>
2024-07-24 23:05:25 -0400

    created login page and added auth routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,27 +1,36 @@
 import express from 'express';
 import User from '../models/userModel.js'
 import createUser from '../controllers/userController.js'
+import passport from 'passport';
 
 const router = express.Router()
 
+// dashboard
 router.get('/', (req, res) => {
     res.json({msg: 'a dummy dashboard'});
 })
 
+// auth login
 router.get('/login', (req, res) => {
     res.render('login');
-    res.json({msg: 'Login'});
 })
 
+// auth logout
 router.get('/logout', (req, res) => {
     // handle with passport
     res.send({msg: 'Logout'});
 })
 
-router.get('/google', (req, res) => {
-    //handle with passport
-    res.send('logging in with google');
+// auth with google
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile']
+}));
+
+// callback route for google to redirect
+router.get('/google/redirect', (req,res) => {
+    res.send('You reached the callback URI')
 })
+
 
 router.get('/signin', (req, res) => {
     res.render('LoginPage');
